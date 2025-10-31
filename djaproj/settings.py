339bcb1c844b89472331b0fd4dd9f347ffe131e7
SandeepSreekailas <sandeepsreekailas123@gmail.com.com>
2025-10-31 12:22:28 +0530

    updated

--- djaproj/settings.py
+++ djaproj/settings.py
@@ -165,14 +165,16 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # MEDIA_URL='/media/'
 # MEDIA_ROOT=BASE_DIR / 'media'
 
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
-
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
-
-}
+# Initialize Cloudinary config
+cloudinary.config(
+    cloud_name=os.getenv('CLOUDINARY_CLOUD_NAME'),
+    api_key=os.getenv('CLOUDINARY_API_KEY'),
+    api_secret=os.getenv('CLOUDINARY_API_SECRET')
+)
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
